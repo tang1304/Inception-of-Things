f7c693175a78a4cf84a49e829a6ed3b270a8b4ac
tgellon <tanguy.gellon@gmail.com>
2025-10-14 12:13:04 +0200

    [WIP] P2: starting pods correctly, now need ingress/hosts

--- p2/scripts/k3s_server_setup.sh
+++ p2/scripts/k3s_server_setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 curl -sfL https://get.k3s.io | sh -s - server --node-ip 192.168.56.110
+
+echo "Waiting 10s for k3s installation to complete..."
+sleep 10
+
+sudo kubectl apply -f /vagrant/confs/app1/app1.yaml
+sudo kubectl apply -f /vagrant/confs/app2/app2.yaml
+sudo kubectl apply -f /vagrant/confs/app3/app3.yaml
+
+sudo kubectl get all
